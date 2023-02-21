590d9ac6fc5cd3c47f7001f0825dac716bd64c4d
Roman Reed <romanreed@Romans-MacBook-Air.local>
2023-02-21 14:28:11 -0500

    Need to fix photos and formating

---
+++ src/tuiter/post-summary-list/post-summary-item.js
@@ -0,0 +1,23 @@
+import React from "react";
+const PostSummaryItem = (
+    {
+      post = { topic: 'Topic', userName: 'User Name', time: 'Time', title: 'Title', tweets: 'Tweets', image: 'Image'}
+    }
+   ) => {
+    return(
+    <li className="list-group-item d-flex ps-3 p-2 justify-content-between">
+        <div>
+            <div style={{ color: 'gray' }}>${post.topic}</div> 
+            <b>${post.userName}</b> <i className="fas fa-check-circle"></i>
+            <span style={{ color: 'gray' }}> - ${post.time}</span><br/>
+            <b>${post.title}</b> 
+            <div style={{ color: 'gray' }}>${post.tweets ? post.tweets : ''} ${post.tweets ? "Tweets": ''}</div>
+        </div>
+        <div>
+            <img src="${post.image}" className="rounded" style={{width: 90, height: 90}}/>
+        </div>
+    </li>
+    );
+  };
+export default PostSummaryItem;
+
