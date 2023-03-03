a2e5116cb105820f3b4eef853f6f9e6ee4b8aa65
romanreed <dblrbk@gmail.com>
2023-03-03 17:03:36 -0500

    fixed rest of warnings

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -12,23 +12,23 @@ const ExploreComponent = () => {
                        wd-nudge-up"></i>
        </div>
        <div className="col-1">
-         <i className="wd-bottom-4 text-primary float-end bi 
+         <i className="wd-bottom-4 text-primary float-end bi
                        bi-gear-fill fs-2 position-relative"></i>
        </div>
      </div>
      <ul className="nav nav-pills mb-2">
        <li className="nav-item">
-         <a className="nav-link active">For You</a>
+         <a href="/#" className="nav-link active">For You</a>
        </li>
        <li className="nav-item">
-         <a className="nav-link">Trending</a>
+         <a href="/#" className="nav-link">Trending</a>
        </li>
        <li className="nav-item">
-         <a className="nav-link">News</a>
+         <a href="/#"className="nav-link">News</a>
        </li>
      </ul>
      <div className="position-relative mb-2">
-       <img src="/images/starshipSpacex.jpg" className="w-100"/>
+       <img alt="main" src="/images/starshipSpacex.jpg" className="w-100"/>
        <h1 className="position-absolute wd-nudge-up text-white">
          SpaceX Starship</h1>
      </div>
